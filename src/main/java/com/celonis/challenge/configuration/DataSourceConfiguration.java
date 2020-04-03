e45c34f6969397e3e20dceb6c5cab1b6565196fa
Felix Wolff <f.wolff@celonis.com>
2020-04-03 23:04:03 +0200

    Use environment variables to configure the application
    Serve those environment variables as base64 encoded kubernetes secrets

---
+++ src/main/java/com/celonis/challenge/configuration/DataSourceConfiguration.java
@@ -0,0 +1,24 @@
+package com.celonis.challenge.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfiguration {
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public DataSource getDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.url(env.getProperty("spring.datasource.url"));
+        dataSourceBuilder.username(System.getenv("DB_USER"));
+        dataSourceBuilder.password(System.getenv("DB_PASSWORD"));
+        return dataSourceBuilder.build();
+    }
+}
